2a38abfd52d749e7cbad20bd275dcb9fc33b4851
Katlyn <kw17611@email.vccs.edu>
2024-11-23 22:09:28 +0000

    code

--- Ch5Ex23.cpp
+++ Ch5Ex23.cpp
@@ -13,6 +13,32 @@ void PatternB();
 
 
 int main(){
+PatternA();
+cout << endl;
+PatternB();
+}
 
+void PatternA(){
+  cout << "Pattern A" << endl
+    << "---------" << endl;
+
+  for (int a = 1; a <= 10; a++){
+    for (int b = 0; b < a; b++){
+      cout << "+";}
+    cout << endl;
   
+    
+} 
+
+  }
+
+void PatternB(){
+ cout << "Pattern B" << endl
+ << "---------" << endl;
+  
+  for (int c = 10; c >= 1; c--){
+     for (int d = 0; d < c; d++){
+       cout << "+";}
+   cout << endl;
+}
 }
